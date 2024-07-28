b25f2c95285105b7fcb07be0b6b2c021628f348b
tienphuc <tienphuc.vlb@gmail.com>
2024-07-29 00:01:45 +0700

    hello

---
+++ DRESSING_ROOM/models/index.js
@@ -0,0 +1,286 @@
+let arrItem = {
+    bikinitop: [
+        {
+            "id": "topcloth_1",
+            "type": "topclothes",
+            "name": "Top Cloth 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth1_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth1.png"
+        },
+        {
+            "id": "topcloth_2",
+            "type": "topclothes",
+            "name": "Top Cloth 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth2_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth2.png"
+        },
+        {
+            "id": "topcloth_3",
+            "type": "topclothes",
+            "name": "Top Cloth 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth3_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth3.png"
+        },
+        {
+            "id": "topcloth_4",
+            "type": "topclothes",
+            "name": "Top Cloth 4",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth4_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth4.png"
+        },
+        {
+            "id": "topcloth_5",
+            "type": "topclothes",
+            "name": "Top Cloth 5",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth5_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth5.png"
+        },
+        {
+            "id": "topcloth_6",
+            "type": "topclothes",
+            "name": "Top Cloth ",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/topcloth6_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/topcloth6.png"
+        },
+    ],
+    bikinibottom: [
+        {
+            "id": "botcloth_1",
+            "type": "botclothes",
+            "name": "Bot Cloth 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/botcloth1_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/botcloth1.png"
+        },
+        {
+            "id": "botcloth_2",
+            "type": "botclothes",
+            "name": "Bot Cloth 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/botcloth2_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/botcloth2.png"
+        },
+        {
+            "id": "botcloth_3",
+            "type": "botclothes",
+            "name": "Bot Cloth 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/botcloth3_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/botcloth3.png"
+        },
+        {
+            "id": "botcloth_4",
+            "type": "botclothes",
+            "name": "Bot Cloth 4",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/botcloth4_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/botcloth4.png"
+        },
+        {
+            "id": "botcloth_5",
+            "type": "botclothes",
+            "name": "Bot Cloth 5",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/clothes/botcloth5_show.jpg",
+            "imgSrc_png": "../assets/images/clothes/botcloth5.png"
+        },
+    ],
+    feet: [
+        {
+            "id": "shoes_1",
+            "type": "shoes",
+            "name": "Shoes 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/shoes/shoes1_show.jpg",
+            "imgSrc_png": "../assets/images/shoes/shoes1.png"
+        },
+        {
+            "id": "shoes_2",
+            "type": "shoes",
+            "name": "Shoes 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/shoes/shoes2_show.jpg",
+            "imgSrc_png": "../assets/images/shoes/shoes2.png"
+        },
+        {
+            "id": "shoes_3",
+            "type": "shoes",
+            "name": "Shoes 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/shoes/shoes3_show.jpg",
+            "imgSrc_png": "../assets/images/shoes/shoes3.png"
+        },
+        {
+            "id": "shoes_4",
+            "type": "shoes",
+            "name": "Shoes 4",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/shoes/shoes4_show.jpg",
+            "imgSrc_png": "../assets/images/shoes/shoes4.png"
+        },
+        {
+            "id": "shoes_5",
+            "type": "shoes",
+            "name": "Shoes 5",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/shoes/shoes5_show.jpg",
+            "imgSrc_png": "../assets/images/shoes/shoes5.png"
+        },
+    ],
+    handbag: [
+        {
+            "id": "handbag_1",
+            "type": "handbags",
+            "name": "Handbag 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/handbags/handbag1_show.jpg",
+            "imgSrc_png": "../assets/images/handbags/handbag1.png"
+        },
+        {
+            "id": "handbag_2",
+            "type": "handbags",
+            "name": "Handbag 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/handbags/handbag2_show.jpg",
+            "imgSrc_png": "../assets/images/handbags/handbag2.png"
+        },
+        {
+            "id": "handbag_3",
+            "type": "handbags",
+            "name": "Handbag 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/handbags/handbag3_show.jpg",
+            "imgSrc_png": "../assets/images/handbags/handbag3.png"
+        },
+    ],
+    necklace: [
+        {
+            "id": "necklace_1",
+            "type": "necklaces",
+            "name": "Necklace 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/necklaces/necklace1_show.jpg",
+            "imgSrc_png": "../assets/images/necklaces/necklace1.png"
+        },
+        {
+            "id": "necklace_2",
+            "type": "necklaces",
+            "name": "Necklace 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/necklaces/necklace2_show.jpg",
+            "imgSrc_png": "../assets/images/necklaces/necklace2.png"
+        },
+        {
+            "id": "necklace_3",
+            "type": "necklaces",
+            "name": "Necklace 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/necklaces/necklace3_show.jpg",
+            "imgSrc_png": "../assets/images/necklaces/necklace3.png"
+        },
+    ],
+    hairstyle: [
+        {
+            "id": "hairstyle_1",
+            "type": "hairstyle",
+            "name": "Hairstyle 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/hairstyle/hairstyle1_show.jpg",
+            "imgSrc_png": "../assets/images/hairstyle/hairstyle1.png"
+        },
+        {
+            "id": "hairstyle_2",
+            "type": "hairstyle",
+            "name": "Hairstyle 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/hairstyle/hairstyle2_show.jpg",
+            "imgSrc_png": "../assets/images/hairstyle/hairstyle2.png"
+        },
+        {
+            "id": "hairstyle_3",
+            "type": "hairstyle",
+            "name": "Hairstyle 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/hairstyle/hairstyle3_show.jpg",
+            "imgSrc_png": "../assets/images/hairstyle/hairstyle3.png"
+        },
+    ],
+    background: [
+        {
+            "id": "background_1",
+            "type": "background",
+            "name": "Background 1",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/background/background1_show.jpg",
+            "imgSrc_png": "../assets/images/background/background1.jpg"
+        },
+        {
+            "id": "background_2",
+            "type": "background",
+            "name": "Background 2",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/background/background2_show.jpg",
+            "imgSrc_png": "../assets/images/background/background2.jpg"
+        },
+        {
+            "id": "background_3",
+            "type": "background",
+            "name": "Background 3",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/background/background3_show.jpg",
+            "imgSrc_png": "../assets/images/background/background3.jpg"
+        },
+        {
+            "id": "background_4",
+            "type": "background",
+            "name": "Background 4",
+            "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, nulla.",
+            "imgSrc_jpg": "../assets/images/background/background4_show.jpg",
+            "imgSrc_png": "../assets/images/background/background4.jpg"
+        }
+    ]
+}
+
+// render
+const render = (key = 'bikinitop') => {
+    let content = `<div class="row ">`
+    for (item of arrItem[key]) {
+        let { name, imgSrc_jpg, imgSrc_png } = item
+        content += `
+            <div class="col-3">
+              <img src="${imgSrc_jpg}" alt="">
+              <p>${name}</p>
+              <button onclick="tryClo('${key}','${imgSrc_png}')">thử đồ</button>
+            </div>`
+    }
+    content += `</div>`
+    document.querySelector(`.well`).innerHTML = content
+}
+render()
+
+//thử đồ
+const tryClo = (id, src) => {
+    document.querySelector(`.${id}`).innerHTML = `<img src="${src}" alt="">`
+}
+
+// thêm click
+document.addEventListener("DOMContentLoaded", function () {
+    let links = document.querySelectorAll("#tab_clo li");
+
+    links.forEach(function (option) {
+        option.addEventListener("click", function (event) {
+            event.preventDefault();
+            let current = document.querySelector("#tab_clo .active");
+            current.classList.remove("active")
+            this.classList.add("active")
+            let id = this.id
+            render(id)
+        })
+    })
+})
